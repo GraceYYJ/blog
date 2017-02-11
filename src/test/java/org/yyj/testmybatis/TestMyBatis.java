99dc026c0edc5808f84e11eba24724185532d5b0
GraceYYJ <649673739@qq.com>
2017-02-11 21:20:12 +0800

    v3

--- src/test/java/org/yyj/testmybatis/TestMyBatis.java
+++ src/test/java/org/yyj/testmybatis/TestMyBatis.java
@@ -16,8 +16,7 @@ import test.model.User;
 import test.service.IUserService;  
   
 @RunWith(SpringJUnit4ClassRunner.class)     //表示继承了SpringJUnit4ClassRunner类  
-@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})  
-  
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})   
 public class TestMyBatis {  
     private static Logger logger = Logger.getLogger(TestMyBatis.class);  
     @Resource  
